51f5d12b4cab968f04abe0d82ccc89faabf5cca7
Kidoo88 <kidistwondemagegn67@gmail.com>
2021-12-14 16:36:24 +0300

    updated shop schema

--- src/shop/shop.schema.js
+++ src/shop/shop.schema.js
@@ -3,6 +3,14 @@ import { StringType } from '../constant/string_type.js';
 
 const shopSchema = mongoosee.Schema({
   name: StringType,
+  pics: [String],
+  services: [
+    {
+      serviceName: StringType,
+      servicePic: String,
+      servicePrice: StringType,
+    },
+  ],
 });
 
 export const ShopModel = mongoosee.model('Shop', shopSchema);
